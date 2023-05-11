7786d2db821baa4d588016bcfce0aeaa4400d689
Denise-Dichiara <dichiara.d@gmail.com>
2023-05-11 16:42:17 +0200

    fixed html h1

--- myscript.js
+++ myscript.js
@@ -1,1 +1,12 @@
-const username = prompt ('Insert yuor name');
+const userName = prompt ('Insert your name');
+
+
+const userSurname = prompt ('Insert your Surname');
+
+let userFavColor = prompt ('Insert your favourite color');
+
+
+
+console.log(userName);
+
+document.getElementById()
