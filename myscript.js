5c8ed5d725089ede0caadf7245466b8fc9d333da
Denise-Dichiara <dichiara.d@gmail.com>
2023-05-12 15:26:52 +0200

    added number to password

--- myscript.js
+++ myscript.js
@@ -5,7 +5,7 @@ const userSurname = prompt ('Insert your Surname');
 
 let userFavColor = prompt ('Insert your favourite color');
 
-let UserInfo = userName + userSurname + userFavColor;
+let UserInfo = userName + userSurname + userFavColor + '23';
 
 document.getElementById('name-surname-fav-color') .innerHTML = UserInfo;
 
